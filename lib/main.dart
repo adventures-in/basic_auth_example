1a3412db24e8ccdaa427eab2cc7f2e9f0458b403
rebornin3d <rebornin3d@gmail.com>
2022-02-01 03:01:14 +0100

    google auth done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/auth.dart';
 
 import 'firebase_options.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MaterialApp(home: Text('hello')));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: AuthGate(),
+    );
+  }
+}
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        // User is not signed in
+        if (!snapshot.hasData) {
+          return const SignInScreen(providerConfigs: [
+            EmailProviderConfiguration(),
+            GoogleProviderConfiguration(
+              clientId: '1:989241975669:web:4953f941e9e51c778fe93c',
+            ),
+          ]);
+        }
+
+        // Render your application if authenticated
+        return Text('Hello ${(snapshot.data as User).displayName}');
+      },
+    );
+  }
 }
